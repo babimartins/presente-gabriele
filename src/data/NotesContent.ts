f538c5273dadf7ce2fac91928d0d074a07c05905
Bárbara <martins.barbara04@gmail.com>
2020-07-23 18:23:57 -0300

    Add notes and photo file

--- src/data/NotesContent.ts
+++ src/data/NotesContent.ts
@@ -6,6 +6,12 @@ export interface NoteContentInterface {
 export default () => {
   const notesContent: NoteContentInterface[] = [
     {
+      content: `Ei Gabizinha! Passando só pra te desejar um feliz aniversário e tudo de bom que esse mundo tem 
+      pra oferecer. Você é uma pessoa cheia de luz e tenho muita sorte de poder ter te conhecido. Espero que 
+      mesmo com a distância você se sinta abraçada e seu dia seja top. Abração!`,
+      signature: 'Lana',
+    },
+    {
       content: `Eii gabizinha. Só passando aqui pra te dizer o quão incrível, inteligente, talentosa e carismática você 
         é. Você é uma pessoa iluminada que eu amei ter conhecido nesses 3 aninhos de cefet. Quero te desejar tudo 
         de melhor nesse mundo pq vc merece demaisss. Beijos te adoro muito muito mesmo.`,
@@ -18,11 +24,25 @@ export default () => {
       signature: 'Ana Beatriz',
     },
     {
+      content: `Então.... passando para te dar parabéns! Eu torço para que tds os seus problemas se resolvam e 
+      desejo tudo de melhor pra vc. Descobrimos q temos mais em comum do que imaginávamos (kk)e tem sido muito 
+      bom ter vc como amiga durante todos esses anos. Enfim, feliz aniversário e como não sei finalizar mensagens 
+      fofinhas, vou terminar essa com uma frase sua: "Mas vc pode smp cv cmg tá? Não sei se posso ajudar muito, 
+      mas pro que precisar eu tô aqui".`,
+      signature: 'Breno',
+    },
+    {
       content: `Amiga, você é uma das pessoas que mais gosto no mundo. Você é cheia de amor e brilho e só quem 
       te conhece sabe o tamanho do seu coração! Eu quero te desejar todas as coisas mais bonitas que existem, 
       muito amor e muita luz (pra poder conseguir enxergar as coisas bonitas lá de trás). Eu te amo e estarei 
       sempre aqui mesmo de longinho. Que você tenha um dia especial, feliz aniversário! Beijos.`,
       signature: 'Eve',
+    },
+    {
+      content: `Parabeeeeens Gabi!!! Te desejo muitas coisas boas, que vc a cada se sinta mais feliz! Espero 
+      ainda poder comemorar muitos aniversários seu junto com vc!! Você é uma mulher maravilhosa, merece curtir 
+      muito esse seu dia! Aproveite bastante! Te amo! Saudades!`,
+      signature: 'Luiza',
     },
     {
       content: `Feeeeeliz vidaaaa Gabiiiiii! Hoje são exatamente 19 anos da sua existência. Caramba... eu tô tão 
@@ -44,24 +64,6 @@ export default () => {
       perca isso de vista. Ah e só pra te lembrar: "tu te tornas eternamente responsável por aquilo que tu cativas". 
       Sendo assim, que venham mais e mais vidas para compartilharmos!`,
       signature: 'Nathália (3)',
-    },
-    {
-      content: `Ei Gabizinha! Passando só pra te desejar um feliz aniversário e tudo de bom que esse mundo tem 
-      pra oferecer. Você é uma pessoa cheia de luz e tenho muita sorte de poder ter te conhecido. Espero que 
-      mesmo com a distância você se sinta abraçada e seu dia seja top. Abração!`,
-      signature: 'Lana',
-    },
-    {
-      content: `Parabeeeeens Gabi!!! Te desejo muitas coisas boas, que vc a cada se sinta mais feliz! Espero 
-      ainda poder comemorar muitos aniversários seu junto com vc!! Você é uma mulher maravilhosa, merece curtir 
-      muito esse seu dia! Aproveite bastante! Te amo! Saudades!`,
-      signature: 'Luiza',
-    },
-    {
-      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.`,
-      signature: 'Signature',
     },
     {
       content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
