91c5e24da33b55f04eb23014afcfa3294b869b06
Bárbara <martins.barbara04@gmail.com>
2020-07-23 10:42:38 -0300

    Create notesContent data

---
+++ src/data/NotesContent.ts
@@ -0,0 +1,106 @@
+export interface NoteContentInterface {
+  content: string;
+  signature: string;
+}
+
+export default () => {
+  const notesContent: NoteContentInterface[] = [
+    {
+      content: `Eii gabizinha. Só passando aqui pra te dizer o quão incrível, inteligente, talentosa e carismática você 
+        é. Você é uma pessoa iluminada que eu amei ter conhecido nesses 3 aninhos de cefet. Quero te desejar tudo 
+        de melhor nesse mundo pq vc merece demaisss. Beijos te adoro muito muito mesmo.`,
+      signature: 'João Vitor',
+    },
+    {
+      content: `Feliz aniversário Gabizinha, te desejo tudo de melhor que o mundo possa te oferecer e queria te 
+        falar que adoro você, uma das pessoas mais incríveis que eu pude conhecer no cefet. Aproveita muito seu 
+        aniversário, tô morrendo de saudades, espero poder te dar um abraço logo, beijãoooo.`,
+      signature: 'Ana Beatriz',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+    {
+      content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.`,
+      signature: 'Signature',
+    },
+  ];
+  return notesContent;
+};
