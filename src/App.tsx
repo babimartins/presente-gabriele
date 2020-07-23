97b3df9e82035e9b028739c2719dd523ad70f2b6
Bárbara <martins.barbara04@gmail.com>
2020-07-23 18:23:45 -0300

    Extract PhotosTab to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Tabs, NotesTab } from './components';
-import { getNotesContent, getPhotosContent } from './data';
+import { Tabs, NotesTab, PhotosTab } from './components';
+import { getNotesContent } from './data';
 import { Grid } from '@material-ui/core';
-import Carousel from 'react-material-ui-carousel';
 
 function App() {
   const headerImage = require('./images/header.png');
-
-  var items = [];
-  const photos = getPhotosContent();
-
-  for (let i = 0; i < photos.length; i += 3) {
-    items.push({ a: photos[i], b: photos[i + 1], c: photos[i + 2] });
-  }
-
-  function Item(props: any) {
-    const imgA = require(`./images/photos/${props.item.a.path.toString()}`);
-    const imgB = require(`./images/photos/${props.item.b.path.toString()}`);
-    const imgC = require(`./images/photos/${props.item.c.path.toString()}`);
-    return (
-      <Grid container spacing={1} style={{ minHeight: '50vh' }}>
-        <Grid item xs={4}>
-          <img src={imgA} alt={props.item.a.path.replace('.jpg', '')} style={{ maxHeight: '45vh' }} />
-        </Grid>
-        <Grid item xs={4}>
-          <img src={imgB} alt={props.item.b.path.replace('.jpg', '')} style={{ maxHeight: '45vh' }} />
-        </Grid>
-        <Grid item xs={4}>
-          <img src={imgC} alt={props.item.c.path.replace('.jpg', '')} style={{ maxHeight: '45vh' }} />
-        </Grid>
-      </Grid>
-    );
-  }
 
   return (
     <Grid container className="App">
@@ -41,17 +14,7 @@ function App() {
       </Grid>
       <Grid item style={{ height: '65vh', width: '100%' }}>
         <Tabs
-          tabsContent={[
-            <div />,
-            <NotesTab notesContent={getNotesContent()} />,
-            <Carousel
-              indicatorProps={{ className: '', style: { padding: '10px' } }}
-              activeIndicatorProps={{ className: '', style: { padding: '10px' } }}>
-              {items.map((item, i) => (
-                <Item key={i} item={item} />
-              ))}
-            </Carousel>,
-          ]}
+          tabsContent={[<div />, <NotesTab notesContent={getNotesContent()} />, <PhotosTab />]}
           tabsLabels={['A Gabi é música', 'Recadinhos', 'Fotinhas']}
         />
       </Grid>
---
+++ src/components/PhotosTab/PhotosTab.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { getPhotosContent } from '../../data';
+import { Grid } from '@material-ui/core';
+import Carousel from 'react-material-ui-carousel';
+
+export default () => {
+  var photosContent: any[] = [];
+  const photos = getPhotosContent();
+
+  for (let i = 0; i < photos.length; i += 3) {
+    photosContent.push({ a: photos[i], b: photos[i + 1], c: photos[i + 2] });
+  }
+
+  const renderPhotos = (items: any[]) => {
+    return items.map((item, i) => {
+      return <Item key={i} item={item} />;
+    });
+  };
+
+  function Item(props: any) {
+    const imgA = require(`../../images/photos/${props.item.a.path.toString()}`);
+    const imgB = require(`../../images/photos/${props.item.b.path.toString()}`);
+    const imgC = require(`../../images/photos/${props.item.c.path.toString()}`);
+    return (
+      <Grid container spacing={1} style={{ minHeight: '50vh' }}>
+        <Grid item xs={4}>
+          <img src={imgA} alt={props.item.a.path.replace('.jpg', '')} style={{ maxHeight: '45vh' }} />
+        </Grid>
+        <Grid item xs={4}>
+          <img src={imgB} alt={props.item.b.path.replace('.jpg', '')} style={{ maxHeight: '45vh' }} />
+        </Grid>
+        <Grid item xs={4}>
+          <img src={imgC} alt={props.item.c.path.replace('.jpg', '')} style={{ maxHeight: '45vh' }} />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  return (
+    <Carousel
+      indicatorProps={{ className: '', style: { padding: '10px' } }}
+      activeIndicatorProps={{ className: '', style: { padding: '10px' } }}>
+      {renderPhotos(photosContent)}
+    </Carousel>
+  );
+};
